f51f7cb447cf08e6dc2f0d04be9f18fc54768fe5
Md Ashique Ali <143936710+ashique6465@users.noreply.github.com>
2025-12-17 11:42:40 +0000

    Added inventory and cart check test case

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# cypress-automation
+# Cypress Automation Project – SauceDemo
+ 
+## Tech Stack
+- Cypress
+- JavaScript
+- GitHub Codespaces
+ 
+## Test Coverage
+### UI Automation
+- Login with valid credentials
+- Login with invalid credentials
+- Inventory & Add to Cart flow
+ 
+## Framework Design
+- Page Object Model (POM)
+- Fixture-based test data
+- Headless execution support
+ 
+## How to Run Tests
+```bash
+npm install
+npx cypress run
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,7 +1,9 @@
 import LoginPage from "../pages/LoginPage";
- 
+import InventoryPage from "../pages/InventoryPage";
+
 describe('SauceDemo Login Tests(POM + Fixture)', ()=>{
     const loginpage = new LoginPage();
+    const inventorypage = new InventoryPage();
     beforeEach(()=>{
         cy.clearCookies()
         cy.clearLocalStorage()
@@ -13,6 +15,13 @@ describe('SauceDemo Login Tests(POM + Fixture)', ()=>{
         loginpage.enterPassword(this.data.validUser.password)
         loginpage.clickLogin()
         loginpage.verifyLoginSuccess()
+
+
+        // Inventroy actions
+        inventorypage.addFirstProductToCart()
+        inventorypage.verifyCartCount(1)
+        inventorypage.openCart()
+        inventorypage.verifyProductInCart()
     })
    it("Login with invalid credentials", function(){
     loginpage.enterUsername(this.data.invalidUser.username)
