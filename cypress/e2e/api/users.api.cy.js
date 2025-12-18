9f652f85feda47228ec83103aa6e129d9355a7a4
Md Ashique Ali <143936710+ashique6465@users.noreply.github.com>
2025-12-18 07:23:15 +0000

    comment down the reqres

--- cypress/e2e/api/users.api.cy.js
+++ cypress/e2e/api/users.api.cy.js
@@ -1,20 +1,20 @@
 
-//It is checking correctly but due to cloudflare protection it give a response status 403.
+// //It is checking correctly but due to cloudflare protection it give a response status 403.
 
-describe('ReqRes API Tests', () =>{
-    it('GET - list user', () =>{
-        cy.request({
-            method: "GET",
-            url: "https://reqres.in/api/users?page=2",
-            headers:{
-                'accept': 'application/json',
-                'user-agent': 'Mozilla/5.0'
-            }
-        }).then((response) =>{
-            expect(response.status).to.eq(200)
-            expect(response.body.page).to.eq(2)
-            expect(response.body.data.length).to.be.greaterThan(0)
-            expect(response.body.data[0]).to.have.property('email')
-        })
-    })
-})
+// describe('ReqRes API Tests', () =>{
+//     it('GET - list user', () =>{
+//         cy.request({
+//             method: "GET",
+//             url: "https://reqres.in/api/users?page=2",
+//             headers:{
+//                 'accept': 'application/json',
+//                 'user-agent': 'Mozilla/5.0'
+//             }
+//         }).then((response) =>{
+//             expect(response.status).to.eq(200)
+//             expect(response.body.page).to.eq(2)
+//             expect(response.body.data.length).to.be.greaterThan(0)
+//             expect(response.body.data[0]).to.have.property('email')
+//         })
+//     })
+// })
